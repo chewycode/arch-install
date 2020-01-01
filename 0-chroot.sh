6db0ec60b48b665adcac38bd91f022e30929b1cc
chewycode <randalneel@gmail.com>
2020-01-01 17:15:03 -0600

    Update 0-chroot.sh

--- 0-chroot.sh
+++ 0-chroot.sh
@@ -1,29 +1,36 @@
 #!/usr/bin/env bash
-#this runs after arch-chroot
+# this runs after you enter arch-chroot
 
+# get user input for computer name
 read -p "Enter computer name: " computername
 if [[ -z "$computername" ]]; then
    printf '%s\n' "No computer name entered. Exiting..."
    exit 1
 fi
 
+# update mirror list
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
+# set locale stuff (change for your info)
 ln -sf /usr/share/zoneinfo/US/Central /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 
+# install grub
 grub-install --recheck --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# enable dhcpcd
 systemctl enable dhcpcd
 
+# set root password
 echo "Enter root password..."
 passwd
 
+# set up network info
 echo "$computername" > /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
