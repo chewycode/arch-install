7abf0b2c3fd5839e085db9d4c4d286aad3d202c2
chewycode <randalneel@gmail.com>
2020-01-12 22:39:00 -0600

    Create virtualbox.sh

---
+++ virtualbox.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo
+echo "Choose (2) virtualbox-guest-modules-arch"
+echo
+pacman -S virtualbox-guest-utils linux-headers
+echo
+echo "Make sure graphics controller is set to VBoxSVGA in order for resolution to auto-adjust."
+echo
