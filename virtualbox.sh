1af02f2c7225b61351ca6b7b3f006dc40514f9f6
chewycode <randalneel@gmail.com>
2020-01-12 22:48:02 -0600

    Update virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -3,6 +3,7 @@ echo
 echo "Choose (2) virtualbox-guest-modules-arch"
 echo
 pacman -S virtualbox-guest-utils linux-headers --noconfirm --needed
+systemctl enable vboxservice.service --now
 echo
 echo "Make sure graphics controller is set to VBoxSVGA in order for resolution to auto-adjust."
 echo
