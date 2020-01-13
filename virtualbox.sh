cf2a02a13820350229d4d976007b8afaeab9a3a0
chewycode <randalneel@gmail.com>
2020-01-12 22:50:33 -0600

    Update virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -6,4 +6,4 @@ pacman -S virtualbox-guest-utils linux-headers --noconfirm --needed
 systemctl enable vboxservice.service --now
 echo
 echo "Make sure graphics controller is set to VBoxSVGA in order for resolution to auto-adjust."
-echo
+echo "In virtualbox menu go to Devices >> Shared Clipboard and select bidirectional"
