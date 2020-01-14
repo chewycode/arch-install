6182782fe79152e29016771b9024e70f666af2dc
chewycode <randalneel@gmail.com>
2020-01-13 23:22:00 -0600

    Update virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -5,6 +5,7 @@ echo
 pacman -S virtualbox-guest-utils linux-headers --noconfirm --needed
 systemctl enable vboxservice.service --now
 echo
+# copy picom file for virtualbox
 sudo cp /root/arch-install/config/picom.conf $HOME/.config/picom.conf
 echo "Make sure graphics controller is set to VBoxSVGA in order for resolution to auto-adjust."
 echo "In virtualbox menu go to Devices >> Shared Clipboard and select bidirectional"
