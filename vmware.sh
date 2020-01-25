fdad24aa3ba0f3874e4f160012c39bcb567dbdbd
chewycode <randalneel@gmail.com>
2020-01-24 22:54:48 -0600

    Create vmware.sh

---
+++ vmware.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# install xorg
+sudo pacman -S open-vm-tools xf86-input-vmmouse xf86-video-vmware mesa -noconfirm -needed
+
+# enable services
+sudo systemctl enable --now vmtoolsd.service
+sudo systemctl enable --now vmware-vmblock-fuse.service
+
+# sync time with host
+vmware-toolbox-cmd timesync enable
