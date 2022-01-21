0281daacaba1e0148a2fd7330faa772e8b3aa45e
chewycode <r@neel.email>
2022-01-21 09:25:36 -0600

    fixed mirrorlist

--- chroot.sh
+++ chroot.sh
@@ -52,7 +52,7 @@ fi
 # update mirror list
 echo "Optimizing mirrorlist"
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+echo "Server = http://mirrors.advancedhosters.com/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist
 echo
 
 # set locale stuff (change for your info)
